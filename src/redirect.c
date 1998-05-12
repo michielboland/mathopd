3a822c8602067e58a25c1b77212295b28f01d1a9
Michiel Boland <michiel@boland.org>
1998-05-12 12:32:00 +0000

    added redirect.c

---
+++ src/redirect.c
@@ -0,0 +1,39 @@
+#include "mathopd.h"
+
+int process_redirect(struct request *r)
+{
+	static char location[PATHLEN];
+	char *c;
+	FILE *fp;
+
+	if (r->method != M_GET && r->method != M_HEAD) {
+		r->error = "invalid method for redirect";
+		return 405;
+	}
+
+	if ((fp = fopen(r->path_translated, "r")) == 0) {
+		lerror("fopen");
+		r->error = "cannot open redirect file";
+		return 500;
+	}
+
+	fgets(location, sizeof location, fp);
+	fclose(fp);
+
+	c = strchr(location, '\n');
+	if (c)
+		*c = '\0';
+	else {
+		r->error = "redirect url too long";
+		return 500;
+	}
+
+	if (strncmp(location, "http://", 7)) {
+		r->error = "can only redirect to http urls";
+		return 500;
+	}
+
+	escape_url(location);
+	r->location = location;
+	return 302;
+}
