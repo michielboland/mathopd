29914f5db91d5c03f0c3f9251921f84cf4d222a9
Michiel Boland <michiel@boland.org>
1998-08-24 17:54:18 +0000

    abuse r->path_args instead of using static array

--- src/redirect.c
+++ src/redirect.c
@@ -2,24 +2,21 @@
 
 int process_redirect(struct request *r)
 {
-	static char location[PATHLEN];
-	char *c;
+	char *location, *c;
 	FILE *fp;
 
+	location = r->path_args;
 	if (r->method != M_GET && r->method != M_HEAD) {
 		r->error = "invalid method for redirect";
 		return 405;
 	}
-
 	if ((fp = fopen(r->path_translated, "r")) == 0) {
 		lerror("fopen");
 		r->error = "cannot open redirect file";
 		return 500;
 	}
-
-	fgets(location, sizeof location, fp);
+	fgets(location, STRLEN, fp);
 	fclose(fp);
-
 	c = strchr(location, '\n');
 	if (c)
 		*c = '\0';
@@ -27,12 +24,10 @@ int process_redirect(struct request *r)
 		r->error = "redirect url too long";
 		return 500;
 	}
-
 	if (strncmp(location, "http://", 7)) {
 		r->error = "can only redirect to http urls";
 		return 500;
 	}
-
 	escape_url(location);
 	r->location = location;
 	return 302;
