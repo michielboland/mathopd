d037c47b6952617bd9ce1f3036b5a37c0e47b4ac
Michiel Boland <michiel@boland.org>
1998-09-02 19:57:52 +0000

    cosmetic cleanup

--- src/dump.c
+++ src/dump.c
@@ -42,9 +42,7 @@
 static void dump_children(struct virtual *v)
 {
 	while (v) {
-		log(L_DEBUG, "dump_children(%p)", v);
-		log(L_LOG, "VHB %s %lu %lu",
-		    v->fullname, v->nrequests, v->nwritten);
+		log(L_LOG, "VHB %s %lu %lu", v->fullname, v->nrequests, v->nwritten);
 		v = v->next;
 	}
 }
@@ -52,10 +50,7 @@ static void dump_children(struct virtual *v)
 static void dump_servers(struct server *s)
 {
 	while (s) {
-		log(L_DEBUG, "dump_servers(%p)", s);
-		log(L_LOG, "SAH %s:%d %lu %lu",
-		    inet_ntoa(s->addr), s->port,
-		    s->naccepts, s->nhandled);
+		log(L_LOG, "SAH %s:%d %lu %lu", inet_ntoa(s->addr), s->port, s->naccepts, s->nhandled);
 		dump_children(s->children);
 		s = s->next;
 	}
@@ -63,15 +58,10 @@ static void dump_servers(struct server *s)
 
 void dump(void)
 {
-	char *ti;
-
 	ti = ctime(&current_time);
-	log(L_LOG, "*** Dump at %.24s (pid %d)\n"
-	    "SCM %lu %lu %d",
-	    ti, my_pid,
-	    startuptime, current_time, maxconnections);
+	log(L_LOG, "*** Dump at %.24s (pid %d)", ctime(&current_time), my_pid);
+	log(L_LOG, "SCM %lu %lu %d", startuptime, current_time, maxconnections);
 	maxconnections = nconnections;
-
 	dump_servers(servers);
 	log(L_LOG, "*** End of dump (pid %d)", my_pid);
 }
