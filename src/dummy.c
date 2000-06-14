5cdaaf10b049d94406921176834396795ba9de50
Michiel Boland <michiel@boland.org>
2000-06-14 23:10:13 +0000

    Added 'All rights reserved' clause.

--- src/dummy.c
+++ src/dummy.c
@@ -1,5 +1,6 @@
 /*
  *   Copyright 1999 Michiel Boland.
+ *   All rights reserved.
  *
  *   Redistribution and use in source and binary forms, with or
  *   without modification, are permitted provided that the following
--- src/mathopd.h
+++ src/mathopd.h
@@ -1,5 +1,6 @@
 /*
  *   Copyright 1996, 1997, 1998, 1999, 2000 Michiel Boland.
+ *   All rights reserved.
  *
  *   Redistribution and use in source and binary forms, with or
  *   without modification, are permitted provided that the following
