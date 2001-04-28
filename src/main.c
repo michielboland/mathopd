c49873c476e822a57836f986c25cd66580ef8ada
Michiel Boland <michiel@boland.org>
2001-04-28 21:09:09 +0000

    Version bump to 1.4b10

--- src/main.c
+++ src/main.c
@@ -53,7 +53,7 @@ static const char rcsid[] = "$Id$";
 #include <stdarg.h>
 #include "mathopd.h"
 
-const char server_version[] = "Mathopd/1.4b9";
+const char server_version[] = "Mathopd/1.4b10";
 
 volatile int gotsigterm;
 volatile int gotsighup;
