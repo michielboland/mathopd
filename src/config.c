ff88e224b59eed3056308b62fa764a9d7007c844
Michiel Boland <michiel@boland.org>
2001-06-04 13:48:53 +0000

    Cast is unnecessary.

--- src/config.c
+++ src/config.c
@@ -192,7 +192,7 @@ int inet_aton(const char *cp, struct in_addr *pin)
 	unsigned long ia;
 
 	ia = inet_addr(cp);
-	if (ia == (unsigned long) -1)
+	if (ia == -1)
 		return 0;
 	pin->s_addr = ia;
 	return 1;
