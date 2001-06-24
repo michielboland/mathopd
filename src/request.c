8c9ea56097a97677d1744348b6c2ff25193a195a
Michiel Boland <michiel@boland.org>
2001-06-24 16:24:31 +0000

    Log get_path_info() failures

--- src/request.c
+++ src/request.c
@@ -734,8 +734,10 @@ static int process_path(struct request *r)
 		r->error_file = r->c->error_404_file;
 		return 404;
 	}
-	if (get_path_info(r) == -1)
+	if (get_path_info(r) == -1) {
+		log_d("get_path_info failed for %s", r->path_translated);
 		return 500;
+	}
 	if (S_ISDIR(r->finfo.st_mode)) {
 		if (r->path_args[0] != '/')
 			return makedir(r);
