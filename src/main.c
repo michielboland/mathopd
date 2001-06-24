955b22c805c2f6f017ef707256797e683e551507
Michiel Boland <michiel@boland.org>
2001-06-24 15:46:18 +0000

    Set file flags once

--- src/main.c
+++ src/main.c
@@ -317,13 +317,10 @@ int fork_request(struct request *r, int (*f)(struct request *))
 				efd = fd;
 			}
 		}
+		fcntl(fd, F_SETFL, 0);
 		dup2(fd, 0);
 		dup2(fd, 1);
 		dup2(efd, 2);
-		fcntl(0, F_SETFL, 0);
-		fcntl(1, F_SETFL, 0);
-		if (efd == fd)
-			fcntl(2, F_SETFL, 0);
 		close(fd);
 		if (efd != fd)
 			close(efd);
