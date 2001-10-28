1d9baa933f0bf76df65c6dea319c66092d809bf0
Michiel Boland <michiel@boland.org>
2001-10-28 22:40:31 +0000

    include <string.h> for strcpy()

--- src/dump.c
+++ src/dump.c
@@ -43,6 +43,7 @@ static const char rcsid[] = "$Id$";
 #include <arpa/inet.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include <unistd.h>
 #include <fcntl.h>
 #include "mathopd.h"
