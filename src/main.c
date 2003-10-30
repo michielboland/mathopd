7372dee9a87f31acbeb50aecf1f80c9038eab95f
Michiel Boland <michiel@boland.org>
2003-10-30 16:04:21 +0000

    Bump version number.

--- src/main.c
+++ src/main.c
@@ -55,7 +55,7 @@ static const char rcsid[] = "$Id$";
 #include <stdlib.h>
 #include "mathopd.h"
 
-const char server_version[] = "Mathopd/1.5b8";
+const char server_version[] = "Mathopd/1.5b9";
 
 volatile sig_atomic_t gotsigterm;
 volatile sig_atomic_t gotsighup;
