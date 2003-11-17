bbde263a882286823c4c56f47f5c75a6df02ec64
Michiel Boland <michiel@boland.org>
2003-11-17 12:16:56 +0000

    Remove unneeded header files. Add sys/stat.h for stat() prototype (this is done via mathopd.h but one should not cound on this.)

--- src/dump.c
+++ src/dump.c
@@ -41,11 +41,10 @@ static const char rcsid[] = "$Id$";
 #include <sys/socket.h>
 #include <sys/time.h>
 #include <sys/resource.h>
+#include <sys/stat.h>
 #include <netinet/in.h>
 #include <arpa/inet.h>
 #include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
 #include <unistd.h>
 #include <fcntl.h>
 #include "mathopd.h"
