f291a31348ccbde6077760c6f35d160fdc4d9bab
Michiel Boland <michiel@boland.org>
2003-11-19 12:59:05 +0000

    Back out the last change.

--- src/stub.c
+++ src/stub.c
@@ -432,7 +432,7 @@ static int readfromclient(struct pipe_params *p)
 		p->imax -= r;
 		break;
 	}
-	return r;
+	return 0;
 }
 
 static int readfromchild(struct pipe_params *p)
@@ -481,7 +481,7 @@ static int readfromchild(struct pipe_params *p)
 		}
 		break;
 	}
-	return r;
+	return 0;
 }
 
 static int writetoclient(struct pipe_params *p)
@@ -518,7 +518,7 @@ static int writetoclient(struct pipe_params *p)
 			p->obp = p->otop = 0;
 		break;
 	}
-	return r;
+	return 0;
 }
 
 static int writetochild(struct pipe_params *p)
@@ -549,7 +549,7 @@ static int writetochild(struct pipe_params *p)
 			p->opp = p->ibp = 0;
 		break;
 	}
-	return r;
+	return 0;
 }
 
 static int scanlflf(struct pipe_params *p)
