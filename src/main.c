47e7350c182fd009b6dc25a2fc7262a76db28d73
Michiel Boland <michiel@boland.org>
2003-11-21 00:59:26 +0000

    Include <time.h> for time prototype.

--- src/main.c
+++ src/main.c
@@ -51,6 +51,7 @@ static const char rcsid[] = "$Id$";
 #include <string.h>
 #include <stdarg.h>
 #include <stdlib.h>
+#include <time.h>
 #include "mathopd.h"
 
 const char server_version[] = "Mathopd/1.5b11";
