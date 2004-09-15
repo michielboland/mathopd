4864735a4bea48f23940bd79a4b07fed2a1aae1d
Michiel Boland <michiel@boland.org>
2004-09-15 17:33:29 +0000

    The content_type member of a request struct is probably not constant, strictly speaking.

--- src/mathopd.h
+++ src/mathopd.h
@@ -226,7 +226,7 @@ struct request {
 	char path[PATHLEN];
 	char path_translated[PATHLEN];
 	char path_args[PATHLEN];
-	const char *content_type;
+	char *content_type;
 	int num_content;
 	int class;
 	long content_length;
