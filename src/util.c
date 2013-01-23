f5da711c3603b31503dc1c23dd3cc0c573f0d224
Michiel Boland <michiel@boland.org>
2013-01-23 13:58:47 +0000

    Change the ipv6_literal variable to inside_ipv6_literal

--- src/util.c
+++ src/util.c
@@ -85,11 +85,11 @@ int unescape_url_n(const char *from, char *to, size_t n)
 void sanitize_host(char *s)
 {
 	int c, l;
-	int ipv6_literal = 0;
+	int inside_ipv6_literal = 0;
 
 	l = 0;
 	if (s[0] == '[') {
-		ipv6_literal = 1;
+		inside_ipv6_literal = 1;
 	}
 	while ((c = *s) != 0) {
 		/*
@@ -97,12 +97,12 @@ void sanitize_host(char *s)
 		 * number, unless the colon was part of an
 		 * IPv6 literal.
 		 */
-		if (c == ':' && ipv6_literal == 0) {
+		if (c == ':' && inside_ipv6_literal == 0) {
 			*s = 0;
 			break;
 		}
 		if (c == ']') {
-			ipv6_literal = 0;
+			inside_ipv6_literal = 0;
 		}
 		*s++ = tolower(c);
 		l = c;
